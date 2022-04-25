9a752d05955ee3d7ecb1976b187d6ffd6bdd771d
iazadur <iamazadur@gmail.com>
2022-04-25 18:08:38 +0600

    add blog section

--- src/views/pages/home/blog/FromBlog.jsx
+++ src/views/pages/home/blog/FromBlog.jsx
@@ -1,41 +1,100 @@
 import React from 'react';
+import blog1 from '../../../../assets/images/blog/blog1.jpg'
+import blog2 from '../../../../assets/images/blog/blog2.jpg'
+import blog3 from '../../../../assets/images/blog/blog3.jpg'
 
 const FromBlog = () => {
     return (
         <div>
             <section class="text-gray-600 body-font">
                 <div class="container px-5 py-24 mx-auto flex flex-wrap">
-                    <div class="flex w-full mb-20 flex-wrap">
-                        <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Master Cleanse Reliac Heirloom</h1>
-                        <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
-                    </div>
+
                     <div class="flex flex-wrap md:-m-2 -m-1">
                         <div class="flex flex-wrap w-1/2">
-                            <div class="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300"/>
+                            <div class="md:p-2 p-1 w-full flex gap-2">
+                                <div className="">
+                                    <img alt="gallery" class="w-full  object-cover object-center block" src="https://dummyimage.com/100x100" />
+                                </div>
+                                <div className="flex flex-col space-y-1">
+                                    <span>Fashon</span>
+                                    <h4>Qualcomm is developing a Nintendo Switch-like console, report says</h4>
+                                    <div className="flex justify-between">
+                                        <div className="flex">
+                                            <span>14 April 2021</span>
+                                            <span>12M views</span>
+                                        </div>
+                                        <a href="/">Read More</a>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/501x301"/>
+                            <div class="md:p-2 p-1 w-full flex gap-2">
+                                <div className="">
+                                    <img alt="gallery" class="w-full  object-cover object-center block" src="https://dummyimage.com/100x100" />
+                                </div>
+                                <div className="flex flex-col space-y-1">
+                                    <span>Fashon</span>
+                                    <h4>Qualcomm is developing a Nintendo Switch-like console, report says</h4>
+                                    <div className="flex justify-between">
+                                        <div className="flex">
+                                            <span>14 April 2021</span>
+                                            <span>12M views</span>
+                                        </div>
+                                        <a href="/">Read More</a>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="md:p-2 p-1 w-full">
-                                <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://dummyimage.com/600x360"/>
+                            <div class="md:p-2 p-1 w-full flex gap-2">
+                                <div className="">
+                                    <img alt="gallery" class="w-full  object-cover object-center block" src="https://dummyimage.com/100x100" />
+                                </div>
+                                <div className="flex flex-col space-y-1">
+                                    <span>Fashon</span>
+                                    <h4>Qualcomm is developing a Nintendo Switch-like console, report says</h4>
+                                    <div className="flex justify-between">
+                                        <div className="flex">
+                                            <span>14 April 2021</span>
+                                            <span>12M views</span>
+                                        </div>
+                                        <a href="/">Read More</a>
+                                    </div>
+                                </div>
                             </div>
+
                         </div>
                         <div class="flex flex-wrap w-1/2">
-                            <div class="md:p-2 p-1 w-full">
-                                <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://dummyimage.com/601x361"/>
+                            <div class="md:p-2 p-1 w-1/2 relative">
+                                <img alt="gallery" class="w-full object-cover object-center block" src={blog3} />
+                                <div className="absolute top-8 left-6">
+                                    <span>Big Offer</span>
+                                    <h4>Save 20% on Women's socks</h4>
+                                    <a href="/">Shop Now </a>
+                                </div>
                             </div>
-                            <div class="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/502x302"/>
+                            <div class="md:p-2 p-1 w-1/2 space-y-4">
+                                <div className="relative">
+                                    <img alt="gallery" class="w-full object-cover object-center block" src={blog2} />
+                                    <div className="absolute top-4 left-4">
+                                        <span>Big Offer</span>
+                                        <h4>Save 20% on Women's socks</h4>
+                                        <a href="/">Shop Now </a>
+                                    </div>
+                                </div>
+                                <div className="relative">
+                                    <img alt="gallery" class="w-full object-cover object-center block" src={blog1} />
+                                    <div className="absolute top-4 left-1/2">
+                                        <span>Big Offer</span>
+                                        <h4>Save 20% on Women's socks</h4>
+                                        <a href="/">Shop Now </a>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/503x303"/>
-                            </div>
+
                         </div>
+
                     </div>
                 </div>
             </section>
-            
+
         </div>
     );
 };
