ff420845acdbd75d441d0cd71c54f2adaa8b144f
wavebox0607 <89796889+wavebox0607@users.noreply.github.com>
2022-09-01 10:01:00 +0600

    Sheikh (#16)
    * design modify
    
    * design modify
    
    * timer section added
    
    * timer section added
    
    * single product page section added
    
    * single product page section added
    
    * single product page section added
    
    * single product
    
    * Test again
    
    * offer price implement
    
    * offer price implement
    
    * offer price modify
    
    * email section change
    
    * error fix
    
    * error fix
    
    * varient problem solved
    
    * varient problem solved
    
    * varient problem solved
    
    * arrow btn solve
    
    * arrow btn solve
    
    * image modify
    
    * some function and design modify
    
    * single product page modify
    
    Co-authored-by: Rayhan Sheikh <rayhan.sheikh10@gmail.com>

--- src/views/components/utils/ColorSelect.jsx
+++ src/views/components/utils/ColorSelect.jsx
@@ -8,12 +8,11 @@ const ColorSelect = ({ setSelect, select, selectColor, bg, getColor }) => {
             <div onClick={() => {
                 getColor(bg)
                 setSelect(selectColor)
-            }} className={`avatar ${select === selectColor ? "ring-2 ring-[#AD171A] ring-offset-2 rounded-full" : null}`}>
+            }} className={`avatar ${select === selectColor ? "ring-2 ring-[#AD171A] ring-offset-2 rounded-full" : "ring-2 ring-gray-500 ring-offset-2 rounded-full"}`}>
 
-
-                {bg !== 'multicolor' && <div className={`text-neutral-content rounded-full w-8 cursor-pointer `} style={{ backgroundColor: bg }}>
+                {bg !== 'multicolor' && <div className={`text-neutral-content rounded-full w-6 cursor-pointer `} style={{ backgroundColor: bg }}>
                 </div>}
-                {bg === 'multicolor' && <div className={`text-neutral-content bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full w-8 cursor-pointer`}>
+                {bg === 'multicolor' && <div className={`text-neutral-content bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full w-6 cursor-pointer`}>
                 </div>}
 
             </div>
